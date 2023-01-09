099dde4dfb8487d1db3c15465b574983dbe38608
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-01-09 10:13:47 +0300

    final project based on tutorial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# drum-kit
+# JavaScript Drum Kit
+
+## A 9-Key Drum Kit written in Vanilla JavaScript
+
+This is the first project based on the JavaScript30 challenge where we built a fully functional drum kit with 9 keys that plays the sound when a user presses the corresponding key and the key transitions to highlight the sound being played. This projects highlights the following areas:
+
+* CSS Tranforms
+* JS Functions - audio functions, classList, querySelector
+* Transition Events - transitionend
+* KeyBoard Events
+
+
  
--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-window.addEventListener('keydown', function(e){
-    console.log(e);
-});
+function playSound(e){
+    //the keycode is the number associated with the key pressed
+    const keyNum = e.keyCode;
+    
+    //the keycode has to be the same as one of the data keys
+    const audio = document.querySelector(`audio[data-key = "${keyNum}"`)
+
+    //if the keycode is not a data key, the function should stop executing
+    if (!audio) return;
+
+    //playing the sound
+    audio.currentTime = 0; //rewinds the audio to ensure it can be played again
+    audio.play();
+
+    //adding the transition to the corresponding key
+    const key = document.querySelector(`.key[data-key = "${keyNum}"]`)
+    key.classList.add('playing')
+
+}
+
+window.addEventListener('keydown', playSound);
+
+function removeTransition(e) {
+    //check if the property has been transitioned
+    if (e.propertyName !== 'transform')return;
+
+    //remove the transition
+    this.classList.remove('playing')
+}
+
+//get a node list of all the keys in the document
+const keys = document.querySelectorAll('.key')
+
+//add an event listener to each key
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
